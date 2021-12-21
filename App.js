b1ce2339e31895fe781f060a10eeb60019b1db44
UgaleKp <96467344+UgaleKp@users.noreply.github.com>
2021-12-21 21:00:23 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const App = () => {
+  const [style, setStyle] = useState('color1');
+
+  const changeStyle = () => {
+    console.log('you just clicked');
+    setStyle('color2');
+  };
+  return (
+    <>
+      <div>
+        <button className={style} onClick={changeStyle}>
+         Change color
+        </button>
+      </div>
+    </>
+  );
+};
+export default App;
